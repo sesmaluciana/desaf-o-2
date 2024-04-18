f8528b95020a0f93eaf39303255ae952ae8798ad
sesmaluciana <lulyy2013@gmail.com>
2024-04-18 20:41:39 -0300

    hice el js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let nombreProducto = "cafetera"
+let precioUnitario = 500000
+let cantidadDeseada = prompt("Ingrese la cantidad de Cafeteras que desea")
+let costoTotal = precioUnitario * cantidadDeseada
+alert(costoTotal)
